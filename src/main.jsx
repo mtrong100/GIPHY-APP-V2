bb416ba75080a33e96fffee555a48dd204610656
trongsieucapvippro <94774477+trongsieucapvippro@users.noreply.github.com>
2023-03-29 18:17:38 +0700

    asdasd

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/Giphy-app">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
