9da9cdd9901ce27d826bcce453fa7e06b924265d
trongsieucapvippro <94774477+trongsieucapvippro@users.noreply.github.com>
2023-03-30 07:17:07 +0700

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Giphy-app">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
